2fd5fdc856a3431f46eda580781c06dc883df244
Vladimir A <avabramanc@gmail.com>
2020-01-09 00:56:56 +0300

    Added validation props and added lazy load for component: Avatar

--- src/components/Header/ShortInfo/ShortInfo.js
+++ src/components/Header/ShortInfo/ShortInfo.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import Avatar from './Avatar';
+
 import Facts from './Facts';
+
+const Avatar = React.lazy(()=> import('./Avatar'))
 
 class ShortInfo extends React.Component{
     
@@ -12,14 +15,23 @@ class ShortInfo extends React.Component{
     render() {
         return (
             <section className="header__short-info">
-                <Avatar
-                    avatar={this.props.avatar}
-                    alt={this.props.alt}
-                />
+                <React.Suspense fallback={<p>Loading..</p>}>
+                    <Avatar
+                        avatar={this.props.avatar}
+                        alt={this.props.alt}
+                    />
+                </React.Suspense>
+
                 <Facts facts={this.props.facts}/>
             </section>
         )
     }
+}
+
+ShortInfo.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    facts: PropTypes.array.isRequired
 }
 
 export default ShortInfo
